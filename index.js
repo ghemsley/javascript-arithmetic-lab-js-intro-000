782486c3c1910d92c41ddc73a6bbcfbe97c9972b
Graham Hemsley <ghemsley@protonmail.ch>
2020-07-20 03:28:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,18 +25,9 @@ function decrement(n) {
 }
 
 function makeInt(n) {
-  try {
-    return parseInt(n, 10)
-  } catch (error) {
-      return NaN
-    }
-  }
+  return parseInt(n, 10)
 }
 
 function preserveDecimal(n) {
-  try {
-    return parseFloat(n)
-  } catch (error) {
-    return NaN
-  }
+  return parseFloat(n)
 }
